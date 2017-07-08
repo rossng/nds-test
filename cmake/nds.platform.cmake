27afe78e5747eafcda1c0fe8752a82bee1b2cb0e
Ross Gardiner <ross-public@gmx.com>
2017-07-08 19:33:06 +0100

    Start fixing for CLion/Windows/CMake 3.7.2

--- cmake/nds.platform.cmake
+++ cmake/nds.platform.cmake
@@ -5,18 +5,20 @@ set(CMAKE_SYSTEM_PROCESSOR arm-eabi)
 set(DEVKITARM $ENV{DEVKITARM})
 set(DEVKITPRO $ENV{DEVKITPRO})
 
+message(STATUS "DEVKITARM: ${DEVKITARM}/bin")
+
 if(NOT DEVKITARM)
-       message(FATAL_ERROR "Please set DEVKITARM in your environment")
+    message(FATAL_ERROR "Please set DEVKITARM in your environment")
 endif()
 
 set(PREFIX arm-none-eabi)
 
-find_program(CMAKE_C_COMPILER ${PREFIX}-gcc ${DEVKITARM}/bin)
-find_program(CMAKE_CXX_COMPILER ${PREFIX}-g++ ${DEVKITARM}/bin)
-find_program(CMAKE_OBJCOPY ${PREFIX}-objcopy ${DEVKITARM}/bin)
-find_program(CMAKE_OBJDUMP ${PREFIX}-objdump ${DEVKITARM}/bin)
-find_program(CMAKE_LINKER ${PREFIX}-ld ${DEVKITARM}/bin)
-find_program(CMAKE_STRIP ${PREFIX}-strip ${DEVKITARM}/bin)
+find_program(CMAKE_C_COMPILER NAMES ${PREFIX}-gcc PATHS ${DEVKITARM}/bin NO_DEFAULT_PATH)
+find_program(CMAKE_CXX_COMPILER NAMES ${PREFIX}-g++ PATHS ${DEVKITARM}/bin NO_DEFAULT_PATH)
+find_program(CMAKE_OBJCOPY NAMES ${PREFIX}-objcopy PATHS ${DEVKITARM}/bin NO_DEFAULT_PATH)
+find_program(CMAKE_OBJDUMP NAMES ${PREFIX}-objdump PATHS ${DEVKITARM}/bin NO_DEFAULT_PATH)
+find_program(CMAKE_LINKER NAMES ${PREFIX}-ld PATHS ${DEVKITARM}/bin NO_DEFAULT_PATH)
+find_program(CMAKE_STRIP NAMES ${PREFIX}-strip PATHS ${DEVKITARM}/bin NO_DEFAULT_PATH)
 
 set(CMAKE_FIND_ROOT_PATH ${DEVKITARM})
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
